99550f8ce58baf69b16664c6b559bf458a6bab90
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-18 12:18:35 -0500

    created comments controller to add/remove comments

---
+++ controllers/comment-controllers.js
@@ -0,0 +1,53 @@
+const { Comment, Pizza } = require('../models');
+
+const commentController = {
+    // add comment to pizza
+    addComment({ params, body }, res) {
+        console.log(body);
+        Comment.create(body)
+            .then(({ _id }) =>{
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $push: { comments: _id } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                // if there is no pizza with targeted id, let'm know
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                } // else - show 'em the zaa
+                res.json(dbPizzaData);
+            })
+            // Error? let'm know!
+            .catch(err => escape.json(err));
+    },
+    removeComment({ params }, res ) {
+        // target a comment with its id and delete
+        Comment.findOneAndDelete({ _id: params.commentId })
+            .then(deletedComment => {
+                // if no comment was deleted, let'm know!
+                if (!deletedComment) {
+                    return res.status(404).json({ messasge: 'No comment with this id!' });
+                }
+                // update the pizza the comment was associated with to remove the comment
+                return Pizza.findOneAndUpdate(
+                    { _id: params.pizzaId },
+                    { $pull: { comments: params.commentId } },
+                    { new: true }
+                );
+            })
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                // the updated comment (no longer with the comment that is targeted for deletion) is turned into JSON here
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    }
+};
+
+module.exports = commentController;
