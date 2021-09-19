695c0380aaf5db85c8e0774d3bb8f7eb0e0c7a72
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-19 15:25:04 -0500

    18.3.5 cmnt-cntrllrs now w/ add/remov cmnt replies

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -21,7 +21,22 @@ const commentController = {
                 res.json(dbPizzaData);
             })
             // Error? let'm know!
-            .catch(err => escape.json(err));
+            .catch(err => res.json(err));
+    },
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+                { _id: params.commentId },
+                { $push: {replies: body } },
+                { new: true }
+            )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id!' });
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
     },
     removeComment({ params }, res ) {
         // target a comment with its id and delete
@@ -46,6 +61,15 @@ const commentController = {
                 // the updated comment (no longer with the comment that is targeted for deletion) is turned into JSON here
                 res.json(dbPizzaData);
             })
+            .catch(err => res.json(err));
+    },
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+                { _id: params.commentId },
+                { $pull: { replies: {replyId: params.replyId } } },
+                { new: true }
+            )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     }
 };
