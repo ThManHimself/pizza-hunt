c47cd61365f2953f2fa667a9d3fbaeaa0fb747ba
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-09-19 15:23:15 -0500

    18.3.5 added routes to add/remove comment replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,12 +1,33 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
 
-// 'add comment' routes -- /api/comments/<pizzaId>
-router.route('/:pizzaId').post(addComment);
+// 'add comment' routes -- /api/comments/:pizzaId
+router
+    .route('/:pizzaId')
+    .post(addComment);
 
-// 'delete comment' routes -- /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// 'add reply' rountes -- /api/comments/:pizzaId/:commentId(this doesnt just add a reply, this deleted the comment with the reply and re-posts it with the new reply attatched)
+router
+    .route('/:pizzaId/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+
+// 'delete comment' routes -- /api/comments/:pizzaId/:commentId
+router
+    .route('/:pizzaId/:commentId')
+    .delete(removeComment);
+
+// 'delete reply' routes -- /api/comments/:pizzaId/:commentId/:replyId
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 
 // exporting... ... ...
